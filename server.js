dbb1c3f6768883d640c663a50e837cd9f6c00c0d
hodayaBar <hodayahalewa973@gmail.com>
2022-12-22 14:50:00 +0200

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-let express=require('express');
+const express=require('express');
 
+const path=require('path');
 let app=express();
 
 app.use(express.static(__dirname+'/dist/shopping-cart'));
 
-app.get('*',(req,resp)=>{
-  resp.sendFile(__dirname+'/dist/shopping-cart/index.html')
+app.get('/*',(req,resp)=>{
+  resp.sendFile(path.join+(__dirname+'/dist/shopping-cart/index.html'));
 });
 
 app.listen(process.env.PORT ||8080);
